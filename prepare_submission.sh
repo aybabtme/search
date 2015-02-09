35e5ead506e4da8db90819e84e0086b480d61b8a
Antoine Grondin <antoinegrondin@gmail.com>
2015-02-08 22:15:31 -0500

    add submission scriptt

---
+++ prepare_submission.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+zip -r 6276497_5650066_asg1.zip \
+    cmd \
+    document \
+    index \
+    preprocess \
+    query \
+    ranking \
+    similarity \
+    term \
+    results.txt \
+    README.md \
+    trec_score_linux \
+    trec_score_os_x \
+    trec_score_win.exe \
